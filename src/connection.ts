477d67e6319066845dffba077b812e7eb86e5957
Chris Kelly <eurochriskelly@gmail.com>
2024-07-02 21:10:50 +0200

    Refactor the project to start with a predefined environment if available
    I'm using ml environents locally, which ever one is current should
    automatically be used in the current terminal

---
+++ src/connection.ts
@@ -0,0 +1,17 @@
+import { MlClientParameters, buildNewClient } from './marklogicClient';
+
+export const createClientConnection = () => {
+    const params: MlClientParameters = {
+        host: process.env['ML_HOST'] || 'localhost',
+        user: process.env['ML_USER'] || 'admin',
+        pwd: process.env['ML_PASS'] || 'admin',
+        port: process.env['ML_PORT'] || '8000',
+        contentDb: process.env['ML_DATABASE'] || 'Documents',
+        modulesDb: process.env['ML_MODULES_DB'] || 'Modules',
+        authType: 'digest',
+        ssl: process.env['ML_PROTOCOL'] === 'https',
+        pathToCa: '',
+        rejectUnauthorized: false
+    };
+    return buildNewClient(params);
+};
